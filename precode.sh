6b8720b0240df5efe4eecf618763623435a8b6c8
KravchenkoArtemV <artem-k-art@yandex.ru>
2024-11-10 11:04:32 +0300

    2nd commit

--- precode.sh
+++ precode.sh
@@ -21,33 +21,33 @@ echo '#!/bin/bash' > dir2/hello.sh
 echo 'echo "$1, привет!"' >> dir2/hello.sh
 
 # устанавливаем для task/dir2/hello.sh права rwxrw-r--
-chmod 765 hello.sh
+chmod 765 dir2/hello.sh
 
 # сохраняем список файлов task/dir2 в task/dir2/list.txt
-touch list.txt && ls > list.txt
+ls dir2 > dir2/list.txt
 
 # копируем содержимое каталога task/dir2 в каталог task/dir3/dir4
-cp -r ../dir2 ../dir3/dir4
+cp -r dir2/* dir3/dir4
 
 # записываем в task/dir1/summary.txt список файлов с расширением *.txt
 # находящихся в task, включая поддиректории
-cd ../dir1 && touch summary.txt && find ../../ -type f -name "*.txt" > summary.txt
+find . -name "*.txt" > dir1/summary.txt
 
 # дописываем в task/dir1/summary.txt содержимое task/dir2/list.txt
-cat ../dir2/list.txt >> summary.txt
+cat dir2/list.txt >> dir1/summary.txt
 
 # определяем переменную окружения NAME со значением "Всем студентам"
 export NAME="Всем студентам"
 
 # запускаем task/dir2/hello.sh с переменной окружения NAME в качестве аргумента
 # вывод скрипта должен дописаться в файл task/dir1/summary.txt
-../dir2/hello.sh "$NAME" >>summary.txt
+dir2/hello.sh "$NAME" >> dir1/summary.txt
 
 # перемещаем с переименованием task/dir1/summary.txt в task/Практическое задание
-mv summary.txt "../Практическое задание"
+mv dir1/summary.txt "Практическое задание"
 
 # выводим на консоль содержимое файла task/Практическое задание
-cat "../Практическое задание"
+cat "Практическое задание"
 
 # ищем в файле "Практическое задание" строки, которые содержат слово "dir"
 # и затем отсортировываем их
